2274b2709a8038481c802aa8cad0c530622b482b
AnnaSKrz <61684482+AnnaSKrz@users.noreply.github.com>
2021-06-12 23:10:59 +0200

    Delete src directory

--- src/ending.js
+++
@@ -1,9 +0,0 @@
-
-function showEndingWindow(){
-    document.getElementById('flightBooking').value = ' >> Rezerwuję... << ';
-    alert('Rezerwacja trwa...');
-}
-
-module.exports  = {
-    showEndingWindow
-}
